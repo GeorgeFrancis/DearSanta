9300dbee072883ce2750f890c9255c308639aa43
georgefrancis1987 <george.francis1987@googlemail.com>
2015-02-24 12:08:54 +0000

    added changes

---
+++ CustomTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.h
+//  BabyDiary
+//
+//  Created by George Francis on 17/02/2015.
+//  Copyright (c) 2015 GeorgeFrancis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UITableViewCell
+
+@end
---
+++ DearSanta/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  myBump
+//
+//  Created by George Francis on 20/02/2015.
+//  Copyright (c) 2015 GeorgeFrancis. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface SettingsViewController : ViewController
+
+@end
